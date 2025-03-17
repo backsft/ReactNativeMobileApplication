52df193d2d3c6fa10d5a5feeb9a635c1156c8c78
kabirmia526@gmail.com <kabirmia526@gmail.com>
2025-03-18 01:58:05 +0600

    color picker

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        primary:'#fc9003',
+        accent:'#AB8BFF'
+      }
+
+    },
   },
   plugins: [],
 }
