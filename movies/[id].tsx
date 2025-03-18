f2d103b827c369a0be56b52d665516afab9cee68
kabirmia526@gmail.com <kabirmia526@gmail.com>
2025-03-18 20:32:14 +0600

    working tabs

---
+++ movies/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet,View, Text } from 'react-native'
+import React from 'react'
+
+const Details = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  )
+}
+
+export default Details;
+
+const Styles=StyleSheet.create({});
